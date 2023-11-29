0141b6d9ff84e9c542124b71d694fdf4be54c985
KarynaStep <stepanova.carina98@gmail.com>
2023-11-29 11:27:44 +0200

    Add store & socket at client

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,6 +3,7 @@ const CONSTANTS = {
     NEW_MESSAGE: "NEW_MESSAGE",
     ERR_MESSAGE: "ERR_MESSAGE",
   },
+  BASE_URL: "localhost:3000",
 };
 
 export default CONSTANTS
--- server/server.js
+++ server/server.js
@@ -2,29 +2,28 @@ const http = require("http");
 const { Server } = require("socket.io");
 const app = require("./app");
 const { WEBSOCKET_EVENTS } = require("./constants");
-const Message = require('./models/Message')
+const Message = require("./models/Message");
 
 const port = process.env.POST || 3000;
 const server = http.createServer(app);
 
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on(WEBSOCKET_EVENTS.NEW_MESSAGE,
-    async (newMessage) => {
-      try {
-        const savedMessage = await Message.create(newMessage);
-        io.emit(WEBSOCKET_EVENTS.NEW_MESSAGE, savedMessage);
-      } catch (error) {
-        socket.emit(WEBSOCKET_EVENTS.ERR_MESSAGE, error);
-      }
-    });
-  socket.on('disconnect', (reason) => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on(WEBSOCKET_EVENTS.NEW_MESSAGE, async (newMessage) => {
+    try {
+      const savedMessage = await Message.create(newMessage);
+      io.emit(WEBSOCKET_EVENTS.NEW_MESSAGE, savedMessage);
+    } catch (error) {
+      socket.emit(WEBSOCKET_EVENTS.ERR_MESSAGE, error);
+    }
+  });
+  socket.on("disconnect", (reason) => {
     console.log(reason);
   });
-})
- 
+});
+
 server.listen(port, () => {
-  console.log("server started at port", port)
-})
+  console.log("server started at port", port);
+});
